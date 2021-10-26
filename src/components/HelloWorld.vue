10d4178bef5425354645a2df3f6931c77a8c712b
YoungHo Choi <0505zxc@gmail.com>
2021-10-27 00:10:45 +0900

    상속 props

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="hello">
-    <my-btn>btn</my-btn>
+
+    <!-- 자삭 컴포넌트의 최상위(루트)가 하나일 경우 상속이 일어남. -->
+    <my-btn style="color: red;">btn</my-btn>
     
     <my-btn-2>
       <template #text> 
@@ -10,6 +12,15 @@
         <span>(a)</span>
       </template>
     </my-btn-2>
+
+    <!-- 상속 -->
+    <MyBtn color="blue"/>
+    <MyBtn color="#000000"/>
+
+    <!-- 불가 -->
+    <!-- <MyBtn color="123"/> 글자가 적혀있는 문자 데이터 x--> 
+    <!-- <MyBtn :color="123"/> 데이터 바인딩을 해서 숫자로 인식되게 해야함.-->
+
   </div>
 </template>
 
--- src/components/MyBtn.vue
+++ src/components/MyBtn.vue
@@ -1,12 +1,23 @@
 <template>
-  <div class="btn">
+  <div class="btn" :style="{ backgroundColor: color }">
 		<slot>btn default</slot>
   </div>
 </template>
 
 <script>
 export default {
-
+	inheritAttrs: false,
+	created() {
+		console.log(this.$attrs)
+	},
+	// MyBtb 컴포넌트가 실행될때 마치 속성처럼 사용가능
+	// 부모-자식간의 데이터 통신!
+	props: {
+		color: {
+			type: String,
+			default: 'gray'
+		}
+	}
 }
 </script>
 
