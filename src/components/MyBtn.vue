d41734d9f3b242bacd57234aee406cbe66a907e9
YoungHo Choi <0505zxc@gmail.com>
2021-10-26 23:29:40 +0900

    slot

---
+++ src/components/MyBtn.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="btn">
+		<slot>btn default</slot>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.btn {
+	display: inline-block;
+	margin: 4px;
+	padding: 6px;
+	border-radius: 4px;
+	background-color: gray;
+	color: white;
+	cursor: pointer;
+}
+</style>
---
+++ src/components/MyBtn2.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="btn">
+		<slot name="icon">(s)</slot>
+		<slot name="text">defalut btn2</slot>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.btn {
+	display: inline-block;
+	margin: 4px;
+	padding: 6px;
+	border-radius: 4px;
+	background-color: gray;
+	color: white;
+	cursor: pointer;
+}
+</style>
